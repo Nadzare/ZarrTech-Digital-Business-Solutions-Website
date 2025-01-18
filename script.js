d2ea96291a9610e269a6cad4636f50e9a2150476
kafah <admin@kafah.com>
2025-01-18 15:43:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function pesanSingkat() {
+    alert("Terimakasih Sudah Ngirim Pesan <3");
+  }
+
+function saranMasukan() {
+    alert("Terimakasih Atas Saran dan Masukannya");
+  }
+
+function aboutLengkap() {
+    document.getElementById("selengkap").style.display = 'inline';
+}
